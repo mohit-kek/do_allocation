0d6030206c26d42ec67287e7c693564f12322aee
m0hity <mohitkumaryadav111@gmail.com>
2022-09-03 02:22:41 +0530

    finish

---
+++ doallocation.js
@@ -0,0 +1,49 @@
+
+// const btn = document.getElementById("btn");
+// const totalBuses = document.querySelector("#totalBuses").value;
+// const totalPassengers = document.querySelector("#totalPassengers").value;
+
+
+// btn.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     console.log(totalBuses);
+//     console.log(totalPassengers);
+// })
+
+
+// const do_allocation = (no_of_passengers, no_of_buses) => {
+//     let passengersCounter = [0, 1];
+//     for (var i = 1; i <= no_of_buses; i++) {
+//         let passenegersInBus;
+
+//         if (i === 1) {
+//             passenegersInBus = 1;
+//         } else if (i => 2) {
+
+//             passenegersInBus = passengersCounter[i - 2] + passengersCounter[i - 1];
+//             passengersCounter.push(passenegersInBus)
+//             no_of_passengers = no_of_passengers - passenegersInBus;
+
+//             if (no_of_passengers <= 0) {
+//                 passenegersInBus = 0;
+//             }
+//         }
+//         console.log(`bus ${i} passengers ${passenegersInBus}`);
+//     }
+// }
+
+
+
+// // do_allocation(totalPassengers, totalBuses);
+
+
+
+
+
+
+
+
+
+
+
+
